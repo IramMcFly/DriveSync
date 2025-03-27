8991eca1a052dbd3fc493b36da5e80cf91a510c6
IramMcFly <124701863+IramMcFly@users.noreply.github.com>
2025-03-27 12:10:10 -0600

    Commit

---
+++ app/asistenciaOnline/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// Components
+import Header from '@/components/viewComponents/Header'
+import Asistencia from '@/components/viewComponents/Asistencia'
+
+const HomeView = () => {
+  return (
+    <>
+        <Header />
+        <Asistencia />
+    </>
+  )
+}
+
+export default HomeView
