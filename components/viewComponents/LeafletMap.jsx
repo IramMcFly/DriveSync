54836e1128b82b83200088ab36a0e0fbff6123e2
IramMcFly <contactomcfly@gmail.com>
2025-04-16 16:59:50 -0600

    repair into map

--- components/viewComponents/LeafletMap.jsx
+++ components/viewComponents/LeafletMap.jsx
@@ -19,18 +19,23 @@ const carIcon = new L.Icon({
   iconAnchor: [18, 36],
 })
 
-const Recenter = ({ coords }) => {
-  const map = useMap()
-  useEffect(() => {
-    if (coords) {
-      map.setView(coords, 14)
-    }
-  }, [coords, map])
-  return null
-}
-
 export default function LeafletMap({ userLocation, assistantLocation }) {
   const [ruta, setRuta] = useState([])
+
+  const Recenter = ({ coords }) => {
+    const map = useMap()
+
+    useEffect(() => {
+      if (coords) {
+        map.setView(coords, 14)
+        setTimeout(() => {
+          map.invalidateSize()
+        }, 0)
+      }
+    }, [coords, map])
+
+    return null
+  }
 
   useEffect(() => {
     const fetchRuta = async () => {
@@ -48,7 +53,9 @@ export default function LeafletMap({ userLocation, assistantLocation }) {
           }
         )
 
-        const coords = response.data.features[0].geometry.coordinates.map(coord => [coord[1], coord[0]])
+        const coords = response.data.features[0].geometry.coordinates.map(
+          (coord) => [coord[1], coord[0]]
+        )
         setRuta(coords)
       } catch (error) {
         console.error("Error obteniendo ruta:", error)
@@ -59,7 +66,12 @@ export default function LeafletMap({ userLocation, assistantLocation }) {
   }, [userLocation, assistantLocation])
 
   return (
-    <MapContainer center={userLocation} zoom={13} scrollWheelZoom={false} className="w-full h-full z-0">
+    <MapContainer
+      center={userLocation}
+      zoom={13}
+      scrollWheelZoom={false}
+      className="w-full h-full z-0"
+    >
       <Recenter coords={userLocation} />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
