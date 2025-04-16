a770a4cbd4275cda4f9fec595d7da120c58e398e
IramMcFly <contactomcfly@gmail.com>
2025-04-16 11:59:36 -0600

    adjust in Service-Form

--- app/formServicios/page.js
+++ app/formServicios/page.js
@@ -1,11 +1,14 @@
 import Header from "@/components/viewComponents/Header"
+import { Suspense } from "react"
 import FormServiciosClient from "@/components/viewComponents/FormServiciosClient"
 
 export default function FormServiciosPage() {
   return (
     <>
       <Header />
-      <FormServiciosClient />
+      <Suspense fallback={<p className="text-white text-center mt-10">Cargando formulario...</p>}>
+        <FormServiciosClient />
+      </Suspense>
     </>
   )
 }
