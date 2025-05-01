b618decf6172fa5d84fd9e5ebc705425b9bd73b0
irammcfly <contactomcfly@gmail.com>
2025-05-01 10:31:04 -0600

    comments

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Login from "@/components/Login";
 import Header from "@/components/viewComponents/Header";
 import ServiciosExpress from "@/components/viewComponents/servicios-express";
 
+// here can be the login into te <Login />
+
 export default function Home() {
   return (
     <>
